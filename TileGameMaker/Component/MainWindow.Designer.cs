9d205677f667b7046d2752ed7eb62212e62db56e
Fernando <fernandoairescastello@gmail.com>
2019-06-14 18:18:43 -0300

    Toggle tool windows visibility through new menu item in main window

--- TileGameMaker/Component/MainWindow.Designer.cs
+++ TileGameMaker/Component/MainWindow.Designer.cs
@@ -33,13 +33,19 @@
             this.FileMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.ExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.StatusBar = new System.Windows.Forms.StatusStrip();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowMapPropertiesMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowPaletteMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowTilesetMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowTemplateMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // MenuBar
             // 
             this.MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.FileMenu});
+            this.FileMenu,
+            this.viewToolStripMenuItem});
             this.MenuBar.Location = new System.Drawing.Point(0, 0);
             this.MenuBar.Name = "MenuBar";
             this.MenuBar.Size = new System.Drawing.Size(747, 24);
@@ -59,7 +65,7 @@
             this.ExitMenuItem.Name = "ExitMenuItem";
             this.ExitMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
             | System.Windows.Forms.Keys.X)));
-            this.ExitMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.ExitMenuItem.Size = new System.Drawing.Size(180, 22);
             this.ExitMenuItem.Text = "Exit";
             this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
             // 
@@ -70,6 +76,53 @@
             this.StatusBar.Size = new System.Drawing.Size(747, 22);
             this.StatusBar.TabIndex = 2;
             this.StatusBar.Text = "statusStrip1";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ShowMapPropertiesMenuItem,
+            this.ShowPaletteMenuItem,
+            this.ShowTilesetMenuItem,
+            this.ShowTemplateMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // ShowMapPropertiesMenuItem
+            // 
+            this.ShowMapPropertiesMenuItem.Checked = true;
+            this.ShowMapPropertiesMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowMapPropertiesMenuItem.Name = "ShowMapPropertiesMenuItem";
+            this.ShowMapPropertiesMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ShowMapPropertiesMenuItem.Text = "Show Map Properties";
+            this.ShowMapPropertiesMenuItem.Click += new System.EventHandler(this.ShowMapPropertiesMenuItem_Click);
+            // 
+            // ShowPaletteMenuItem
+            // 
+            this.ShowPaletteMenuItem.Checked = true;
+            this.ShowPaletteMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowPaletteMenuItem.Name = "ShowPaletteMenuItem";
+            this.ShowPaletteMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ShowPaletteMenuItem.Text = "Show Palette";
+            this.ShowPaletteMenuItem.Click += new System.EventHandler(this.ShowPaletteMenuItem_Click);
+            // 
+            // ShowTilesetMenuItem
+            // 
+            this.ShowTilesetMenuItem.Checked = true;
+            this.ShowTilesetMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowTilesetMenuItem.Name = "ShowTilesetMenuItem";
+            this.ShowTilesetMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ShowTilesetMenuItem.Text = "Show Tileset";
+            this.ShowTilesetMenuItem.Click += new System.EventHandler(this.ShowTilesetMenuItem_Click);
+            // 
+            // ShowTemplateMenuItem
+            // 
+            this.ShowTemplateMenuItem.Checked = true;
+            this.ShowTemplateMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowTemplateMenuItem.Name = "ShowTemplateMenuItem";
+            this.ShowTemplateMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.ShowTemplateMenuItem.Text = "Show Template";
+            this.ShowTemplateMenuItem.Click += new System.EventHandler(this.ShowTemplateMenuItem_Click);
             // 
             // MainWindow
             // 
@@ -98,5 +151,10 @@
         private System.Windows.Forms.StatusStrip StatusBar;
         private System.Windows.Forms.ToolStripMenuItem FileMenu;
         private System.Windows.Forms.ToolStripMenuItem ExitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowMapPropertiesMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowPaletteMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowTilesetMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowTemplateMenuItem;
     }
 }
--- TileGameMaker/Component/MainWindow.cs
+++ TileGameMaker/Component/MainWindow.cs
@@ -15,22 +15,56 @@ namespace TileGameMaker.Component
 {
     public partial class MainWindow : Form
     {
+        private readonly MapEditor MapEditor;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            MapEditor editor = new MapEditor(this);
-            editor.MapWindow.Location = new Point(0, 0);
-            editor.ColorPickerWindow.Location = new Point(800, 0);
-            editor.TilePickerWindow.Location = new Point(1040, 0);
-            editor.TemplateWindow.Location = new Point(800, 365);
-            editor.MapPropertyWindow.Location = new Point(900, 365);
-            editor.Show();
+            MapEditor = new MapEditor(this);
+            MapEditor.MapWindow.Location = new Point(0, 0);
+            MapEditor.ColorPickerWindow.Location = new Point(800, 0);
+            MapEditor.TilePickerWindow.Location = new Point(1040, 0);
+            MapEditor.TemplateWindow.Location = new Point(800, 365);
+            MapEditor.MapPropertyWindow.Location = new Point(900, 365);
+            MapEditor.Show();
         }
 
         private void ExitMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void ShowMapPropertiesMenuItem_Click(object sender, EventArgs e)
+        {
+            ToggleView(MapEditor.MapPropertyWindow, sender);
+        }
+
+        private void ShowPaletteMenuItem_Click(object sender, EventArgs e)
+        {
+            ToggleView(MapEditor.ColorPickerWindow, sender);
+        }
+
+        private void ShowTilesetMenuItem_Click(object sender, EventArgs e)
+        {
+            ToggleView(MapEditor.TilePickerWindow, sender);
+        }
+
+        private void ShowTemplateMenuItem_Click(object sender, EventArgs e)
+        {
+            ToggleView(MapEditor.TemplateWindow, sender);
+        }
+
+        private void ToggleView(Form form, object eventSource)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)eventSource;
+
+            if (form.Visible)
+                form.Hide();
+            else
+                form.Show();
+
+            item.Checked = form.Visible;
         }
     }
 }
