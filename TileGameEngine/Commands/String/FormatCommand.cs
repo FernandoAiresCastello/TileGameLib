0f454a14a4dafa457efe9ab83838db124e791a94
Fernando Machado Aires de Oliveira <fernandoairescastello@gmail.com>
2019-09-04 16:36:04 -0300

    TGML format command

--- TileGameEngine/Commands/String/FormatCommand.cs
+++ TileGameEngine/Commands/String/FormatCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TileGameEngine.Exceptions;
 
 namespace TileGameEngine.Commands.String
 {
@@ -10,9 +11,35 @@ namespace TileGameEngine.Commands.String
     {
         public override void Execute(List<string> immediateParams)
         {
-            string format = PopStr();
+            string formatString = PopStr();
             int number = PopInt();
-            string result = number.ToString(format);
+
+            ScriptException formatException = new ScriptException("Invalid format: " + formatString);
+
+            string[] format = formatString.Split(',');
+            if (format.Length != 2)
+                throw formatException;
+            if (!int.TryParse(format[0].Trim(), out int length))
+                throw formatException;
+            if (length < 1)
+                throw formatException;
+
+            string result = "";
+            string notation = format[1].Trim();
+
+            if (notation == "X" || notation == "x")
+            {
+                result = Convert.ToString(number, 16).PadLeft(length, '0');
+                result = notation == "X" ? result.ToUpper() : result.ToLower();
+            }
+            else if (notation.ToUpper() == "B")
+            {
+                result = Convert.ToString(number, 2).PadLeft(length, '0');
+            }
+            else
+            {
+                throw formatException;
+            }
 
             ParamStack.Push(result);
         }
