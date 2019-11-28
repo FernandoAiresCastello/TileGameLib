38d932734c4075079970903bb26695f8eb106be7
Fernando Machado Aires de Oliveira <fernandoairescastello@gmail.com>
2019-11-28 12:08:58 -0300

    Removed "copyright" symbol from splash screen because it makes no sense

--- TileGameMaker/Windows/SplashWindow.Designer.cs
+++ TileGameMaker/Windows/SplashWindow.Designer.cs
@@ -75,7 +75,7 @@
             this.LbCopyright.Name = "LbCopyright";
             this.LbCopyright.Size = new System.Drawing.Size(455, 50);
             this.LbCopyright.TabIndex = 3;
-            this.LbCopyright.Text = "© 2019\r\nDeveloped by Fernando Aires Castello";
+            this.LbCopyright.Text = "2019\r\nDeveloped by Fernando Aires Castello";
             this.LbCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox1
