ca71f8677381aee8849ca02f6356a8488ca7b9e2
Fernando Machado Aires de Oliveira <fernandoairescastello@gmail.com>
2019-12-24 12:01:08 -0300

    Incomplete JavascriptExporter class

---
+++ TileGameLib/FileExporters/JavascriptExporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TileGameLib.GameElements;
+
+namespace TileGameLib.FileExporters
+{
+    public static class JavascriptExporter
+    {
+        public static void Export(ObjectMap map, string path)
+        {
+            //todo
+            //System.IO.File.WriteAllText();
+        }
+    }
+}
