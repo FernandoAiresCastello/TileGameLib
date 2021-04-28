194ee6db9246eae89329b6ea6afc2c1f225116e2
Fernando <fernandoairescastello@gmail.com>
2021-04-28 15:54:49 -0300

    TemplateObjectList, TemplateObjectView

---
+++ TileGameMaker/Panels/TemplateObjectView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TileGameMaker.MapEditorElements;
+
+namespace TileGameMaker.Panels
+{
+    public partial class TemplateObjectView : UserControl
+    {
+        public TemplateObjectList ObjectList { set; get; }
+        private MapEditor Editor;
+
+        private TemplateObjectView()
+        {
+            InitializeComponent();
+        }
+
+        public TemplateObjectView(MapEditor editor)
+        {
+            Editor = editor;
+        }
+    }
+}
