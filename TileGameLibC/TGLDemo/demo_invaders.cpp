66ff6ed82f6586e63d6f6780418a5f930d6f2cfa
Fernando <fernandoairescastello@gmail.com>
2023-02-27 02:23:37 -0300

    View history

--- TileGameLibC/TGLDemo/demo_invaders.cpp
+++ TileGameLibC/TGLDemo/demo_invaders.cpp
@@ -35,10 +35,6 @@ void demo_invaders()
 	tgl.title("TGL Invaders");
 	tgl.window();
 
-	auto files = tgl.file_list("C:\\Fernando\\Proj\\TileGameLib");
-	auto folders = tgl.folder_list("C:\\Fernando\\Proj\\TileGameLib");
-	auto text = tgl.file_lines("C:\\Fernando\\Proj\\TileGameLib\\.gitattributes");
-
 	init_tiles();
 	init_aliens();
 
