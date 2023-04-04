8b2b970cff77adf0a5c919c737cbee1d9c08b958
Fernando Aires Castello <fernandoairescastello@gmail.com>
2023-04-04 08:14:13 -0300

    View history

--- Other/TileGameLibJS/test.js
+++ Other/TileGameLibJS/test.js
@@ -21,16 +21,22 @@ const tile2 = [
     '#f00','#0f0','#0f0','#0f0','#0f0','#0f0','#0f0','#f00',
     '#f00','#0ff','#f00','#0ff','#f0f','#0ff','#f0f','#0ff'
 ];
-/*
+
+tgl.backColor("#008");
+tgl.cls();
 tgl.colorNormal();
 tgl.drawFree(tile2, 0, 0);
-tgl.colorBinary('#ff0', '#f0f');
+tgl.colorBinary("#ff0", "#f0f");
 tgl.drawTiled(tile, 1, 1);
 tgl.transparency(true);
 tgl.drawFree(tile, 12, 12);
-*/
+tgl.colorBinary("#0f0")
+tgl.printTiled("Hello World!", 4, 10);
+
+/*
 requestAnimationFrame(onFrame);
 function onFrame() {
     tgl.testDrawTiles();
     requestAnimationFrame(onFrame);
 }
+*/
