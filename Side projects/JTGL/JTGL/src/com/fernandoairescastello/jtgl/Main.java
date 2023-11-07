c07b9f6e5cb757bdc8348a894b3b8fd1c227180b
Fernando Aires Castello <fernandoairescastello@gmail.com>
2023-11-07 14:23:56 -0300

    View history

--- Side projects/JTGL/JTGL/src/com/fernandoairescastello/jtgl/Main.java
+++ Side projects/JTGL/JTGL/src/com/fernandoairescastello/jtgl/Main.java
@@ -1,27 +1,8 @@
 package com.fernandoairescastello.jtgl;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferStrategy;
-
 public class Main
 {
     public static void main(String[] args) {
         TGLWindow wnd = new TGLWindow();
-
-        // draw FPS in title
-        new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                wnd.setTitle(Long.toString(wnd.framesDrawn));
-                wnd.framesDrawn = 0;
-            }
-        }).start();
-
-        while (true) {
-            wnd.draw();
-        }
     }
 }
--- Side projects/JTGL/JTGL/src/com/fernandoairescastello/jtgl/TGLWindow.java
+++ Side projects/JTGL/JTGL/src/com/fernandoairescastello/jtgl/TGLWindow.java
@@ -1,64 +1,24 @@
 package com.fernandoairescastello.jtgl;
 
-import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.image.*;
 import javax.swing.*;
-import java.util.Random;
+import java.awt.*;
 
 public class TGLWindow extends JFrame
 {
-	long framesDrawn;
-	final int bufferWidth = 352;
-	final int bufferHeight = 200;
-	final int windowSize = 3;
-	int[] raster;
-	ColorModel cm;
-	DataBuffer buffer;
-	SampleModel sm;
-	WritableRaster wrRaster;
-	BufferedImage backBuffer;
-	Random random = new Random();
-	Graphics gr;
+	final private TGLPanel panel;
 
 	public TGLWindow() {
-		setSize(windowSize * bufferWidth, windowSize * bufferHeight);
+		setSize(800, 600);
+		//setUndecorated(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 		setVisible(true);
 		setResizable(false);
 
-		raster = new int[bufferWidth * bufferHeight];
-		cm = new DirectColorModel(24, 255 << 16, 255 << 8, 255);
-		buffer = new DataBufferInt(raster, raster.length);
-		sm = cm.createCompatibleSampleModel(bufferWidth, bufferHeight);
-		wrRaster = Raster.createWritableRaster(sm, buffer, null);
-		backBuffer = new BufferedImage(cm, wrRaster, false, null);
+		panel = new TGLPanel(this);
 
-		createBufferStrategy(1);
-		BufferStrategy bs = getBufferStrategy();
-		gr = bs.getDrawGraphics();
-
-		addComponentListener(new ComponentAdapter() {
-			public void componentResized(ComponentEvent evt) {
-				Component c = (Component) evt.getSource();
-				draw();
-			}
-		});
-	}
-
-	public void draw()
-	{
-		// produce raster
-		for (int ptr = 0, x = 0; x < bufferWidth; x++) {
-			for (int y = 0; y < bufferHeight; y++) {
-				raster[ptr++] = random.ints(0x000000, 0xffffff)
-						.findFirst().getAsInt();
-			}
-		}
-
-		// draw raster
-		gr.drawImage(backBuffer, 0, 0, getWidth(), getHeight(), null);
-		framesDrawn++;
+		BorderLayout layout = new BorderLayout();
+		setLayout(layout);
+		add(panel, BorderLayout.CENTER);
 	}
 }
